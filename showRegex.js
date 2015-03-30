3f43f9f872cb03dd52883dac685ea6aba0249470
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-03-30 17:05:27 -0400

    answer for showRegex.js

---
+++ showRegex.js
@@ -0,0 +1,14 @@
+//Eric Heinzman
+//Answer for showRegex.js
+//Cmp237
+
+//first way to declare regular expression
+var RegEx_1 = new RegExp("abc");
+
+//second way to declare regular expression
+
+var RegEx_2 = /abc/;
+
+//printing out results
+print(RegEx_1);
+print(RegEx_2);
